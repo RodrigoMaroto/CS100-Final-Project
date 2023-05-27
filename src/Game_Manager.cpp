37d44367b4da0fef70832bedf75e4a2df6da4198
RodrigoMaroto <rmarotocbur@gmail.com>
2023-05-26 18:55:46 -0700

    Added move function; args must be checked outside

---
+++ src/Game_Manager.cpp
@@ -0,0 +1,15 @@
+#include "../header/Chess_Board.h"
+#include "../header/Game_Manager.h"
+#include <iostream>
+#include <string>
+#include <vector>
+
+void Game_Manager::move(vector<int> destination, vector<int> currentLocation){
+    Piece* piece = chessboard.board[currentLocation.at(0)][currentLocation.at(1)];
+    if (chessboard.board[destination.at(0)][destination.at(1)] != nullptr)
+        delete chessboard.board[destination.at(0)][destination.at(1)];
+    chessboard.board[destination.at(0)][destination.at(1)] = piece;
+    chessboard.board[currentLocation.at(0)][currentLocation.at(1)] = nullptr;
+    piece->position = destination;
+    piece->hasMoved = true;
+}
