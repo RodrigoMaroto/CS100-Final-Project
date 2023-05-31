644087d77bb97dd2b5c96decc2c62b5cb73d6b2d
cbrou002 <cbrou002@ucr.edu>
2023-05-31 15:23:08 -0700

    Updated valid destinations for bishop and created main function test

--- src/bishop.cpp
+++ src/bishop.cpp
@@ -15,36 +15,127 @@ vector<vector<int>> Bishop::validDestinations(Piece* board[8][8])
     int row = position.at(0);
     int column = position.at(1);
     vector<vector<int>> allMoves;
-    for(int i = -7; i <= 7 ; ++i)
+
+    // begin bishop-type moves
+    for(int i = 1; i <=7;++i)// up & right
     {
-        if(i != 0)// cant move to current position
+        row = position.at(0);
+        column = position.at(1);
+        row = row + i;
+        column = column + i;
+        if((row >=0 && row <= 7)&&(column >=0 && column <=7))// checks if location is on the board
         {
-            // for diagonals
-            row = row + i;
-            column = column + i;
-            if((row >=0 && row <= 7)&&(column >=0 && column <=7))// checks if location is on the board
+            if(board[row][column] == nullptr)//no piece in this spot
             {
                 pos.at(0) = row;
                 pos.at(1) = column;
                 allMoves.push_back(pos);// add move to list
             }
-            row = position.at(0);
-            column = position.at(1);
-            //top left to bottom right alg for bishop move
-            row = row - i;
-            column = column + i;
-            if((row >=0 && row <= 7)&&(column >=0 && column <=7))// checks if location is on the board
+            else if(board[row][column]->color == this -> color)//piece of same color in way
+            {
+                break;
+            }
+            else if(board[row][column]->color != this->color)//piece of opposite color (can take)
             {
                 pos.at(0) = row;
                 pos.at(1) = column;
                 allMoves.push_back(pos);// add move to list
+                break;
             }
             row = position.at(0);
             column = position.at(1);
         }
     }
+    for(int i = 1; i <=7;++i)// down & right
+    {
+        row = position.at(0);
+        column = position.at(1);
+        row = row - i;
+        column = column +i;
+        if((row >=0 && row <= 7)&&(column >=0 && column <=7))// checks if location is on the board
+        {
+            if(board[row][column] == nullptr)//no piece in this spot
+            {
+                pos.at(0) = row;
+                pos.at(1) = column;
+                allMoves.push_back(pos);// add move to list
+            }
+            else if(board[row][column]->color == this -> color)//piece of same color in way
+            {
+                break;
+            }
+            else if(board[row][column]->color != this->color)//piece of opposite color (can take)
+            {
+                pos.at(0) = row;
+                pos.at(1) = column;
+                allMoves.push_back(pos);// add move to list
+                break;
+            }
+        }
+        row = position.at(0);
+        column = position.at(1);
+    }
+    for(int i = 1; i <=7;++i)// down & left
+    {
+        row = position.at(0);
+        column = position.at(1);
+        row = row - i;
+        column = column -i;
+        if((row >=0 && row <= 7)&&(column >=0 && column <=7))// checks if location is on the board
+        {
+            if(board[row][column] == nullptr)//no piece in this spot
+            {
+                pos.at(0) = row;
+                pos.at(1) = column;
+                allMoves.push_back(pos);// add move to list
+            }
+            else if(board[row][column]->color == this -> color)//piece of same color in way
+            {
+                break;
+            }
+            else if(board[row][column]->color != this->color)//piece of opposite color (can take)
+            {
+                pos.at(0) = row;
+                pos.at(1) = column;
+                allMoves.push_back(pos);// add move to list
+                break;
+            }
+        }
+        row = position.at(0);
+        column = position.at(1);
+    }
+    for(int i = 1; i <=7;++i)// up & left
+    {
+        row = position.at(0);
+        column = position.at(1);
+        row = row + i;
+        column = column - i;
+        if((row >=0 && row <= 7)&&(column >=0 && column <=7))// checks if location is on the board
+        {
+            if(board[row][column] == nullptr)//no piece in this spot
+            {
+                pos.at(0) = row;
+                pos.at(1) = column;
+                allMoves.push_back(pos);// add move to list
+            }
+            else if(board[row][column]->color == this -> color)//piece of same color in way
+            {
+                break;
+            }
+            else if(board[row][column]->color != this->color)//piece of opposite color (can take)
+            {
+                pos.at(0) = row;
+                pos.at(1) = column;
+                allMoves.push_back(pos);// add move to list
+                break;
+            } 
+        }
+        row = position.at(0);
+        column = position.at(1);
+    }
     return allMoves;
 }
+
 string Bishop::getDisplayChar()
 {
     string displayChar = "no char";
