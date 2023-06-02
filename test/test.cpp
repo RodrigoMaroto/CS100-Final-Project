35d41e3beef0c03cd11acd2d12019612ba32c088
RodrigoMaroto <rmarotocbur@gmail.com>
2023-06-02 16:34:18 -0700

    Added CMake files and a mock test

--- test/test.cpp
+++ test/test.cpp
@@ -1,25 +1,14 @@
-#include "../header/pawn.h"
-#include <iostream>
+#include "../header/Game_Manager.h"
+#include "gtest/gtest.h"
 #include <vector>
 
 using namespace std;
 
-int main()//outputs validDestinations for pawn
-{
-    Piece * board[8][8];
-    vector<int> pos;
-    pos.push_back(0);
-    pos.push_back(1);
-    Pawn myPawn('w',pos);
-    vector<vector<int>> allMoves = myPawn.validDestinations(board);
-    for(int i = 0; i < allMoves.size(); i++)
-    {
-        for(int j = 0; j < 2; j++)
-        {
-            cout << allMoves[i][j];
-        }
-        cout << endl;
-    }
-    cout << endl;
-    return 0;
+TEST(MockTest, test1){
+    EXPECT_TRUE(1==1);
+}
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
