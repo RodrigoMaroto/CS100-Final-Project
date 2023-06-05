933a51919d17880b710aa6b0e82c1ea8040e93a3
RodrigoMaroto <rmarotocbur@gmail.com>
2023-06-05 15:05:27 -0700

    Added tests for checkmate and stalemate

--- test/test.cpp
+++ test/test.cpp
@@ -4,7 +4,7 @@
 
 using namespace std;
 
-TEST(CheckMate, stairMate){
+TEST(Checkmate, stairMate){
     Game_Manager game = Game_Manager();
     vector<int> pos;
     pos.push_back(0);
@@ -18,7 +18,7 @@ TEST(CheckMate, stairMate){
     EXPECT_TRUE(game.isCheckMate());
 }
 
-TEST(CheckMate, stairMateStop){
+TEST(Checkmate, stairMateStop){
     Game_Manager game = Game_Manager();
     vector<int> pos;
     pos.push_back(0);
@@ -35,7 +35,7 @@ TEST(CheckMate, stairMateStop){
     EXPECT_FALSE(game.isCheckMate());
 }
 
-TEST(CheckMate, stairMateTooClose){
+TEST(Checkmate, stairMateTooClose){
     Game_Manager game = Game_Manager();
     vector<int> pos;
     pos.push_back(0);
@@ -49,7 +49,7 @@ TEST(CheckMate, stairMateTooClose){
     EXPECT_FALSE(game.isCheckMate());
 }
 
-TEST(CheckMate, onlyStopAlsoCreatesCheck){
+TEST(Checkmate, onlyStopAlsoCreatesCheck){
     Game_Manager game = Game_Manager();
     vector<int> pos;
     pos.push_back(1);
@@ -73,7 +73,7 @@ TEST(CheckMate, onlyStopAlsoCreatesCheck){
     EXPECT_TRUE(game.isCheckMate());
 }
 
-TEST(CheckMate, pawnStop){
+TEST(Checkmate, pawnStop){
     Game_Manager game = Game_Manager();
     vector<int> pos;
     pos.push_back(1);
@@ -94,6 +94,82 @@ TEST(CheckMate, pawnStop){
     pos.at(0) = 4;
     game.chessboard.addPiece(pos, 'b', 'b');
     EXPECT_FALSE(game.isCheckMate());
+}
+
+TEST(Stalemate, QueenCorner){
+    Game_Manager game = Game_Manager();
+    vector<int> pos;
+    pos.push_back(0);
+    pos.push_back(0);
+    game.chessboard.addPiece(pos, 'k', 'w');
+    pos.at(0) = 1;
+    pos.at(1) = 2;
+    game.chessboard.addPiece(pos, 'q', 'b');
+    EXPECT_TRUE(game.isStalemate());
+}
+
+TEST(Stalemate, checkmate){
+    Game_Manager game = Game_Manager();
+    vector<int> pos;
+    pos.push_back(0);
+    pos.push_back(4);
+    game.chessboard.addPiece(pos, 'k', 'w');
+    pos.at(1) = 0;
+    game.chessboard.addPiece(pos, 'r', 'b');
+    pos.at(1) = 1;
+    pos.at(0) = 1;
+    game.chessboard.addPiece(pos, 'r', 'b');
+    EXPECT_FALSE(game.isStalemate());
+}
+
+TEST(Stalemate, QueenCornerAnotherPiece){
+    Game_Manager game = Game_Manager();
+    vector<int> pos;
+    pos.push_back(0);
+    pos.push_back(0);
+    game.chessboard.addPiece(pos, 'k', 'w');
+    pos.at(0) = 1;
+    pos.at(1) = 2;
+    game.chessboard.addPiece(pos, 'q', 'b');
+    pos.at(1) = 7;
+    game.chessboard.addPiece(pos, 'p', 'w');
+    EXPECT_FALSE(game.isStalemate());
+}
+
+TEST(Stalemate, pinnedPiece){
+    Game_Manager game = Game_Manager();
+    vector<int> pos;
+    pos.push_back(0);
+    pos.push_back(0);
+    game.chessboard.addPiece(pos, 'k', 'w');
+    pos.at(0) = 1;
+    pos.at(1) = 2;
+    game.chessboard.addPiece(pos, 'q', 'b');
+    pos.at(0) = 3;
+    pos.at(1) = 0;
+    game.chessboard.addPiece(pos, 'b', 'w');
+    pos.at(0) = 7;
+    pos.at(1) = 0;
+    game.chessboard.addPiece(pos, 'r', 'b');
+    EXPECT_TRUE(game.isStalemate());
+}
+
+TEST(Stalemate, blockedPawn){
+    Game_Manager game = Game_Manager();
+    vector<int> pos;
+    pos.push_back(0);
+    pos.push_back(0);
+    game.chessboard.addPiece(pos, 'k', 'w');
+    pos.at(0) = 1;
+    pos.at(1) = 2;
+    game.chessboard.addPiece(pos, 'q', 'b');
+    pos.at(0) = 4;
+    pos.at(1) = 4;
+    game.chessboard.addPiece(pos, 'p', 'w');
+    pos.at(0) = 5;
+    pos.at(1) = 4;
+    game.chessboard.addPiece(pos, 'p', 'b');
+    EXPECT_TRUE(game.isStalemate());
 }
 
 int main(int argc, char **argv) {
